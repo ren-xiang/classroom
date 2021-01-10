9496117e0e4305d5c97b687c22f9758262e6d588
mzl-2019 <1609180777@qq.com>
2021-01-10 21:25:10 +0800

    mzl

--- src/main/java/Dao/InsertDao.java
+++ src/main/java/Dao/InsertDao.java
@@ -1,22 +1,25 @@
 package Dao;
 
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import util.JDBCUtils;
 import domain.Arrangement;
 
-import java.util.Date;
 import java.util.List;
 
 public class InsertDao {
     JdbcTemplate jdbcTemplate=new JdbcTemplate(JDBCUtils.getDataSource());
-    public void insertArrangement(Arrangement arrangement){
+    
+    public boolean insertArrangement(Arrangement arrangement){
         String sql="insert into arrangement values(?,?,?,?,?,?,?)";
         jdbcTemplate.update(sql,arrangement.getClassroomID(),arrangement.getCid(),arrangement.getTeacherID(),
                 arrangement.getStatus(),arrangement.getWeek(),arrangement.getWeekDay(),arrangement.getSession());
+        return true;
     }
 
-    public void insertArrangement(List<Arrangement> list){
+    public boolean insertArrangement(List<Object[]> list){
         String sql="insert into arrangement values(?,?,?,?,?,?,?)";
-        jdbcTemplate.batchUpdate(sql, String.valueOf(list));
+        jdbcTemplate.batchUpdate(sql, list);
+        return true;
     }
 }
--- src/main/java/Dao/SearchDao.java
+++ src/main/java/Dao/SearchDao.java
@@ -7,6 +7,7 @@ import  domain.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SearchDao {
     JdbcTemplate jdbcTemplate=new JdbcTemplate(JDBCUtils.getDataSource());
@@ -53,4 +54,26 @@ public class SearchDao {
         List<Classroom> list = jdbcTemplate.query(sb_sql, new BeanPropertyRowMapper<>(Classroom.class), params.toArray());
         return list;
     }
+    //按条件查询符合要求的空闲教室
+    public List<Classroom> selectFreeClassroomByCondition(Map<String,String> map,int start, int pageSize){
+        String sql="select * from status_view where 1=1 ";
+        StringBuilder sb=new StringBuilder(sql);
+        List params=new ArrayList();
+        for (String s : map.keySet()) {
+            if(s.equals("minSize")){
+                sb.append(" and snum>="+map.get(s)+" ");
+                continue;
+            }
+            sb.append(" and "+s+" like ? ");
+            params.add("%"+map.get(s)+"%");
+        }
+        sb.append(" limit ? , ? ");
+        String sb_sql=sb.toString();
+        System.out.println(sb_sql);
+        params.add(start);
+        params.add(pageSize);
+        List<Classroom> list = jdbcTemplate.query(sb_sql, new BeanPropertyRowMapper<>(Classroom.class), params.toArray());
+        return list;
+    }
+        
 }
