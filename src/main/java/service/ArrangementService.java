61e43ca165d09e2ba27014e3aaa038161d14f20d
kudlifeMacBook <ljunyfor@outlook.com>
2021-01-11 16:57:42 +0800

    生成安排可能完成了

--- src/main/java/service/ArrangementService.java
+++ src/main/java/service/ArrangementService.java
@@ -1,8 +1,46 @@
 package service;
 import Dao.*;
+import domain.ArrangeClassroom;
+import domain.Arrangement;
+import domain.Course;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrangementService {
     public boolean deleteAllArrangement() {
         DeleteDao dao = new DeleteDao();
         return dao.deleteArrangement();
+    }
+
+    public boolean generateArrangement() {
+        SearchDao searchDao = new SearchDao();
+        List<ArrangeClassroom> arrangeClassroomList = searchDao.searchArraangeClassroom();
+        List<Course> courseList = searchDao.selectCourse();
+        courseList.sort(Comparator.comparingInt(o -> Integer.parseInt(o.getMinRequest())));
+        arrangeClassroomList.sort(Comparator.comparingInt(o -> Integer.parseInt(o.getSnum())));
+        List<Arrangement> result = new LinkedList<>();
+        for (Course course : courseList) {
+            for (int i = 0; i < arrangeClassroomList.size(); i++) {
+                ArrangeClassroom classroom = arrangeClassroomList.get(i);
+                if (Integer.parseInt(classroom.getSnum()) >= Integer.parseInt(course.getMinRequest())) {
+                    if (course.getType().equals(classroom.getType())) {
+                        Arrangement arrangement = new Arrangement();
+                        arrangement.setCid(course.getCid());
+                        arrangement.setClassroomID(classroom.getClassroomID());
+                        arrangement.setTeacherID(course.getTeacherID());
+                        arrangement.setStatus("使用");
+                        arrangement.setWeek("1");
+                        arrangement.setWeekDay(classroom.getWeekDay());
+                        arrangement.setSession(classroom.getSession());
+                        arrangeClassroomList.remove(classroom);
+                        result.add(arrangement);
+                    }
+                }
+            }
+        }
+        return new InsertDao().insertArrangementByProduce(result);
     }
 }
--- src/main/java/servlet/CreateArrangementServlet.java
+++ src/main/java/servlet/CreateArrangementServlet.java
@@ -1,12 +1,30 @@
 package servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import service.ArrangementService;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 生成安排表
  */
 @WebServlet("/createArrangement")
 public class CreateArrangementServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArrangementService service = new ArrangementService();
+        ObjectMapper mapper = new ObjectMapper();
+        resp.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(resp.getOutputStream(), service.generateArrangement());
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
 }
